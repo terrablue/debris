344995de9d51f7aa47f0cb1ccf5ab626a8c275c5
terrablue <102580937+terrablue@users.noreply.github.com>
2022-05-13 17:22:11 +0200

    add bin

---
+++ bin/debris.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import {Path, File} from "runtime-compat";
+import {App} from "debris";
+
+const base = Path.resolve();
+
+const get_config_path = async project_directory => {
+  const filename = "debris.json";
+  const file = new File(project_directory, filename);
+  return await file.exists ? file.path : `../${filename}`;
+}
+
+const conf = import(await get_config_path(base), {assert: {type: "json"}});
+const app = new App(base, (await conf).default);
+await app.load();
+app.run(process.argv[2]);
